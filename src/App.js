f51ea445e2e0b1bc97c885e49757d2ae25d637af
Ujjwal <ujjwalsingh@MBP.local>
2021-01-16 19:44:03 +0530

    v1.1

--- src/App.js
+++ src/App.js
@@ -11,15 +11,34 @@ const searchUrl = `https://api.unsplash.com/search/photos/`
 function App() {
   const [loading, setLoading] = useState(false)
   const [photos, setPhotos] = useState([])
+  const [page, setPage] = useState(0)
+
+  const [query, setQuery] = useState("")
 
   const fetchImages = async () => {
     setLoading(true)
     let url
-    url = `${mainUrl}${clientID}`
+    const urlPage = `&page=${page}`
+    const urlQuery = `&query=${query}`
+    if (query) {
+      url = `${searchUrl}${clientID}${urlPage}${urlQuery}`
+    } else {
+      url = `${mainUrl}${clientID}${urlPage}`
+    }
+
     try {
       const response = await fetch(url)
       const data = await response.json()
-      console.log(data)
+      setPhotos((oldPhotos) => {
+        if (query && page === 1) {
+          return data.results
+        } else if (query) {
+          return [...oldPhotos, ...data.results]
+        } else {
+          return [...oldPhotos, ...data]
+        }
+      })
+      setLoading(false)
     } catch (error) {
       setLoading(false)
       console.log(error)
@@ -28,9 +47,54 @@ function App() {
 
   useEffect(() => {
     fetchImages()
+  }, [page])
+
+  useEffect(() => {
+    const event = window.addEventListener("scroll", () => {
+      if (!loading && window.innerHeight + window.scrollY >= document.body.scrollHeight - 2) {
+        setPage((oldPage) => {
+          return oldPage + 1
+        })
+      }
+    })
+    return () => window.removeEventListener("scroll", event)
   }, [])
 
-  return <h2>stock photos starter</h2>
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setPage(1)
+    fetchImages()
+  }
+
+  return (
+    <main>
+      <section className="search">
+        <form className="search-form">
+          <input
+            type="text"
+            placeholder="search"
+            className="form-input"
+            value={query}
+            onChange={(e) => {
+              setQuery(e.target.value)
+            }}
+          />
+          <button type="submit" className="submit-btn" onClick={handleSubmit}>
+            <FaSearch></FaSearch>
+          </button>
+        </form>
+      </section>
+
+      <section>
+        <div className="photos-center">
+          {photos.map((image) => {
+            return <Photo key={image.id} {...image}></Photo>
+          })}
+        </div>
+        {loading && <h2 className="loading">Loading</h2>}
+      </section>
+    </main>
+  )
 }
 
 export default App
